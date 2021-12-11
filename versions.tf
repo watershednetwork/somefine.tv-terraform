2e5932f69cdbd4d2d859ca1be3640e96e67e906e
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2021-12-10 21:35:56 -0600

    Add S3 bucket for episodes

--- versions.tf
+++ versions.tf
@@ -11,6 +11,11 @@ terraform {
   required_version = ">= 1.0.8"
 
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+
     dnsimple = {
       source  = "dnsimple/dnsimple"
       version = ">= 0.9"
