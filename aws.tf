1ea005d1d5e554cdb5361eecdf94e93496b06aca
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2024-03-25 09:37:05 -0500

    Address deprecation of aws_s3_bucket.acl
    Instead, associate private ACL and BucketOwnerPreferred ownership
    controls configuration, per the documentation.

--- aws.tf
+++ aws.tf
@@ -1,4 +1,17 @@
 resource "aws_s3_bucket" "episodes" {
   bucket = "somefine-tv"
+}
+
+resource "aws_s3_bucket_ownership_controls" "private" {
+  bucket = aws_s3_bucket.episodes.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "episodes-private" {
+  depends_on = [aws_s3_bucket_ownership_controls.private]
+
+  bucket = aws_s3_bucket.episodes.id
   acl    = "private"
 }
